841212fdf71d51c7589fe182a76a7dfc47dc2658
nadzeya <vitserresvit@gmail.com>
2023-07-11 20:01:06 +0300

    style-JS

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const main_title = document.createElement("h3");
 main_title.setAttribute("class", "main-title");
 main_title.innerText = "НАШИ САМЫЕ БОЛЬШИЕ ПРОЕКТЫ"
 main.append(main_title);
+const main_main_cards = document.createElement("div")
+main_main_cards.setAttribute("class", "main-main-cards")
 
 const cards = [["./images/img.main/arena.svg"], ["Арена"], ["Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву."]];
 
@@ -31,4 +33,5 @@ cards_text.setAttribute("class", "cards-text")
 cards_text.innerText = text;
 main_cards.append(cards_text);
 
-main.append(main_cards);}
+main.append(main_main_cards);
+main_main_cards.append(main_cards)}
